0b5c9069591b599667ad7db837b28794b6c3057b
Alexander <asand3r@gmail.com>
2017-11-09 00:23:02 +0300

    Upload new 0.5 version

--- v0.5/zbx_backup_v0.5.0.sh
+++ v0.5/zbx_backup_v0.5.0.sh
@@ -10,11 +10,17 @@
 #
 VERSION="0.5.0"
 
+# Colors ANSI escape codes for color output
+RED='\033[0;31m'			# Red color
+YELLOW='\033[1;33m'			# Yellow color
+BLUE='\033[1;34m'			# Light blue color
+NON_COLOR='\033[0m'			# Reset Color
+
 ### Static setttings ###
 # Working directories and files
 DEST=/mnt/nfs/shv-mon01			# Where we should  store final archive
 TMP=/var/tmp/zbx_backup			# Where to store temp MySQL backup, before it will be compress
-ROTATION=10				# How many old archives we should store
+ROTATION=10				# How many copies we should store. Set to 0, if you needn't rotation.
 LOGFILE=$DEST/backuplog.log		# Logfile location
 TIMESTAMP=`date +%d.%m.%Y.%H%M%S`	# Current timestamp
 ZBX_FILES_TAR=$TMP/zbx_files_$TIMESTAMP.tar
@@ -23,7 +29,7 @@ INNOBACKUPEX=/usr/bin/innobackupex
 
 # Database backup settings
 DB_USER="root"
-DB_PASS=`cat /root/.mysql`
+DB_PASS_FILE=/root/.mysql
 DB_NAME="zabbix"
 
 # Backing up directories
@@ -36,6 +42,7 @@ then
 	mkdir -p $TMP
 fi
 
+# The function just print help message
 function PrintHelpMessage() {
 echo "
 zbx_backup, version: $VERSION
@@ -44,16 +51,19 @@ Usage:
 -c|--compress-with	- gzip|bzip2|xz|lbzip2|pbzip2
 -i|--use-innobackupex	- will use 'innobackupex' utility to backup database
 -m|--use-mysqldump	- will use 'mysqldump' utility to backup database
+-d|--db-only		- backing up database only without Zabbix config files etc
 -h|--help		- print this help message
 -v|--version		- print version number
 
 Example:
 zbx_backup --compress-with lbzip2 --use-innobackupex
+zbx_backup --compress-with gzip --use-mysqldump
+zbx_backup --compress-with xz --db-only
 "
 exit 0
 }
 
-# EXPERIMENTAL Parsing cmd args
+# Parsing given arguments
 if [[ $# -eq 0 ]]
 then
 	echo "Syntax error! Please, provide some arguments. Use '--help' to view examples."
@@ -66,7 +76,7 @@ do
 	ARG="$1"
 	case "$ARG" in
 		"-c"|"--compress-with")
-			C_USED="YES"
+			USE_COMPRESSION="YES"
 			case "$2" in
 				"gzip"|"bzip2"|"xz"|"lbzip2"|"pbzip2")
 					COMPRESS_WITH=$2
@@ -80,15 +90,15 @@ do
 			shift
 			;;
 		"-i"|"--use-innobackupex")
-			I_USED="YES"
-			USE_MYSQLDUMP="NO"
 			USE_INNOBACKUPEX="YES"
 			shift
 			;;
 		"-m"|"--use-mysqldump")
-			M_USED="YES"
 			USE_MYSQLDUMP="YES"
-			USE_INNOBACKUPEX="NO"
+			shift
+			;;
+		"-d"|"--db-only")
+			DB_ONLY="YES"
 			shift
 			;;
 		"-h"|"--help")
@@ -106,59 +116,88 @@ do
 done
 
 # We cannot use both '-m' and '-i' options, so breaks here
-if [[ $I_USED == "YES" ]] && [[ $M_USED == "YES" ]]
+if [[ $USE_INNOBACKUPEX == "YES" ]] && [[ $USE_MYSQLDUMP == "YES" ]]
 then
-	echo "ERROR: You cannot use '-m' and '-i' options together!"
+	echo -e "${RED}ERROR${NON_COLOR}: You cannot use '-m' and '-i' options together!"
 	exit 1
 # Also we should use at least one of them
-elif [[ $I_USED != "YES" ]] && [[ $M_USED != "YES" ]]
+elif [[ $USE_INNOBACKUPEX != "YES" ]] && [[ $USE_MYSQLDUMP != "YES" ]] && [[ $DB_ONLY != "YES" ]]
 then
-	echo "ERROR: You must specify at least one database backup utility. Use '--help' to learn how."
+	echo -e "${RED}ERROR${NON_COLOR}: You must specify at least one database backup utility. Use '--help' to learn how."
 	exit 1
 fi
 
-# Creating sql and file archives
+# The function makes all backup operations
 function BackingUp() {
-	ZBX_FILES_TAR=$TMP/zbx_files_$TIMESTAMP.tar
-	# Making initial files tar archive
-	tar cf $ZBX_FILES_TAR ${ZBX_CATALOGS[0]}
-	
-	# Add all other catalogs in $ZBX_CATALOGS array to initial tar archive
-	if [[ -f $ZBX_FILES_TAR ]]
+	# If '--db-only' option not set
+	if [[ $DB_ONLY != "YES" ]]
 	then
-		for (( i=1; i < ${#ZBX_CATALOGS[@]}; i++ ))
-		do
-			tar -rf $ZBX_FILES_TAR ${ZBX_CATALOGS[$i]}
-		done
-	else
-		echo "ERROR: Cannot create TAR archive with zabbix data files."
-		exit 1
+		ZBX_FILES_TAR=$TMP/zbx_files_$TIMESTAMP.tar
+		# Making initial files tar archive
+		tar cf $ZBX_FILES_TAR ${ZBX_CATALOGS[0]}
+	
+		# Add all other catalogs in $ZBX_CATALOGS array to initial tar archive
+		if [[ -f $ZBX_FILES_TAR ]]
+		then
+			for (( i=1; i < ${#ZBX_CATALOGS[@]}; i++ ))
+			do
+				tar -rf $ZBX_FILES_TAR ${ZBX_CATALOGS[$i]}
+			done
+		else
+			echo -e "${RED}ERROR${NON_COLOR}: Cannot create TAR archive with zabbix data files."
+			exit 1
+		fi
+	fi
+
+	# Check last exit code
+	if [[ $? -ne 0 ]]
+	then
+		echo "ERROR: Cannot create $ZBX_FILES_TAR" >> $LOGFILE
+		return 1
 	fi
 	
-	# Backing up database
-	if [[ $USE_MYSQLDUMP == "YES" ]] && [[ $USE_INNOBACKUPEX == "NO" ]]
-	DB_DUMP=$TMP/zbx_db_dump_$TIMESTAMP.bak
+	# Getting DB password
+	if [[ -f $DB_PASS_FILE ]]
 	then
+		DB_PASS=`cat $DB_PASS_FILE`
+	else
+		echo -e "${RED}ERROR${NON_COLOR}: Cannot open file with database password ($DB_PASS_FILE)"
+		exit 1
+	fi
+
+	# Backing up database
+	# If we want to use mysqldump to backup database
+	if [[ $USE_MYSQLDUMP == "YES" ]]
+	then
+		DB_BACKUP_DST=$TMP/zbx_db_dump_$TIMESTAMP.sql
 		if [[ -f $MYSQLDUMP ]]
 		then
-			$MYSQLDUMP -u$DB_USER -p$DB_PASS --databases "$DB_NAME" > $DB_DUMP
+			$MYSQLDUMP -u$DB_USER -p$DB_PASS --databases "$DB_NAME" > $DB_BACKUP_DST
 		else
-			echo "ERROR: 'mysqldump' utility wasn't found!"
-		fi	
-	elif [[ $USE_MYSQLDUMP == "NO" ]] && [[ $USE_INNOBACKUPEX = "YES" ]]
-	DB_BACKUP_DST=$TMP/zbx_mysql_files_$TIMESTAMP
+			echo -e "${RED}ERROR${NON_COLOR}: 'mysqldump' utility not found ($MYSQLDUMP)."
+			exit 1
+		fi
+	# If we want to use innobackupex to backup database
+	elif [[ $USE_INNOBACKUPEX = "YES" ]]
 	then
+		DB_BACKUP_DST=$TMP/zbx_mysql_files_$TIMESTAMP
 		if [[ -f $INNOBACKUPEX ]]
 		then
 			$INNOBACKUPEX --user=$DB_USER --password=$DB_PASS --no-timestamp $DB_BACKUP_DST
 			$INNOBACKUPEX --apply-log --redo-only --no-timestamp $DB_BACKUP_DST
 		else
+			echo -e "${RED}ERROR${NON_COLOR}: Cannot find 'innobackupex' utility ($INNOBACKUPEX)."
+			exit 1
 		fi
 	fi
-}
 
-# Running BackingUp function
-BackingUp
+	# Chech last exit code
+	if [[ $? -ne 0 ]]
+	then
+		echo "ERROR: Cannot create database backup" >> $LOGFILE
+		return 1
+	fi
+}
 
 # The function cleans $TMP directory
 function TmpClean() {
@@ -172,50 +211,59 @@ function TmpClean() {
 
 # The function making rotation of old backup files
 function RotateOldCopies() {
-	old_copies=(`ls -1t $DEST/zbx_backup_*`)
-	count=${#old_copies[@]}
+	# Getting old copies list and it's count
+	OLD_COPIES=(`ls -1t $DEST/zbx_backup_*`)
+	COUNT=${#old_copies[@]}
 
-	if [[ $count -gt $ROTATION ]]
+	if [[ $COUNT -gt $ROTATION ]] && [[ $ROTATION -ne 0 ]]
 	then
-		for old_copy in ${old_copies[@]:$ROTATION}
+		for OLD_COPY in ${OLD_COPIES[@]:$ROTATION}
 		do
-			if [[ -f $old_copy ]]
+			if [[ -f $OLD_COPY ]]
 			then
-				rm -f $old_copy
+				rm -f $OLD_COPY
 			else
-				echo "WARNING: Something was wrong while deleting $old_file" >> $LOGFILE
+				echo "WARNING: Something was wrong while deleting $OLD_COPY" >> $LOGFILE
 			fi
 		done
 	else
-		echo "INFO: Less or equal $ROTATION copies: $count. Do nothing..." >> $LOGFILE
+		echo "INFO: We have less or equal $ROTATION old copies: $COUNT. Do nothing..." >> $LOGFILE
 	fi
 }
 
-# Cleaning TMP and backing up
+# Cleaning TMP and Running backup operations
 TmpClean && BackingUp
 
-# Compressing if files exist
-if [[ -f $ZBX_FILES_TAR ]] && [[ -d $DB_BACKUP_DST ]]
+# Checking last exit code for backup function
+if [[ $? -ne 0 ]]
 then
-	if [[ $USE_LBZIP2 == 1 ]]
-	then
-		tar cf $FULL_ARC -I lbzip2 $ZBX_FILES_TAR $DB_BACKUP_DST
-	else
-		tar -zacf $FULL_ARC $ZBX_FILES_TAR $DB_BACKUP_DST
-	fi
-else
-	echo "ERROR: Cannot compress all files." >> $LOGFILE
+	echo -e "${RED}ERROR${NON_COLOR}: Backup operation hasn't finished correctly. Look into log file to find posible reason ($LOGFILE)." 
+	exit 1
 fi
 
-# Cheking and logging results
-if [[ -f $FULL_ARC ]]
+# Compressing if resulted files exists
+if [[ $USE_COMPRESSION == "YES" ]]
 then
-	echo "SUCCESS: Backup date: $TIMESTAMP" >> $LOGFILE
-else
-	echo "ERROR: Backup wasn't created on $TIMESTAMP" >> $LOGFILE
+	if [[ $DB_ONLY == "YES" ]]
+	then
+		tar cf $FULL_ARC -I $COMPRESS_WITH $DB_BACKUP_DST
+	elif [[ -f $ZBX_FILES_TAR ]]
+	then
+		tar cf $FULL_ARC -I $COMPRESS_WITH $ZBX_FILES_TAR $DB_BACKUP_DST
+	fi
 fi
 
 # Cleaning temp files and run rotation
 TmpClean && RotateOldCopies
 
-exit 0
+# Cheking and logging results
+if [[ -f $FULL_ARC ]]
+then
+	echo "SUCCESS: Backup date: $TIMESTAMP" >> $LOGFILE
+	exit 0
+else
+	echo "ERROR: Backup wasn't created on $TIMESTAMP" >> $LOGFILE
+	exit 1
+fi
+
+exit $?
