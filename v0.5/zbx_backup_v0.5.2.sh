0a42df83da23c1ece4da15bec4ee2e3730028b71
Khatsayuk Alexander <asand3r@gmail.com>
2017-11-14 16:54:59 +0300

    Add new version - 0.5.2

--- v0.5/zbx_backup_v0.5.2.sh
+++ v0.5/zbx_backup_v0.5.2.sh
@@ -3,28 +3,26 @@
 #
 # This script can help you to create backup of simple Zabbix instance.
 # It makes tar archives of config and scripts directories. Also it makes MySQL backup with
-# Percona Xtrabackup utility (innobackupex) whitch you should install yourself.
+# Percona Xtrabackup utility (xtrabackup) whitch you should install yourself.
 # After all it makes compressed archive contains all collected data using gzip, bzip2 or xz.
 # After a few tests I reccomend to use lbzip2. It makes archive faster, but almost
 # two times bigger than xz. Gzip it fine too, but as bzip2, rather slow (in my case).
 #
-VERSION="0.5.0"
+VERSION="0.5.2"
 
 ### Static setttings ###
 # Working directories and files
-DEST="/mnt/nfs/shv-mon01"			# Where we should  store final archive
 TMP="/var/tmp/zbx_backup"			# Where to store temp MySQL backup, before it will be compress
-ROTATION=10				# How many copies we should store. Set to 0, if you needn't rotation.
-LOGFILE="$DEST/backuplog.log"		# Logfile location
-TIMESTAMP=`date +%d.%m.%Y.%H%M%S`	# Current timestamp
-ZBX_FILES_TAR="$TMP/zbx_files_$TIMESTAMP.tar"
+ROTATION=10					# How many copies we should store. Set to 0, if you needn't rotation.
+TIMESTAMP=`date +%d.%m.%Y.%H%M%S`		# Current timestamp
+#ZBX_FILES_TAR="$TMP/zbx_files_$TIMESTAMP.tar"
 ZBX_CATALOGS=("/usr/lib/zabbix" "/etc/zabbix")
 ### END Static settings ###
 
 # Checking TEMP directory
 if ! [[ -d "$TMP" ]]
 then
-	mkdir -p "$TMP"
+	mkdir -p $TMP
 fi
 
 # The function just print help message
@@ -33,23 +31,24 @@ echo "
 zbx_backup, version: $VERSION
 (c) Khatsayuk Alexander, 2017
 Usage:
+-s|--save-to		- choose location to save result archive file (default: current directory)
 -c|--compress-with	- gzip|bzip2|lbzip2|pbzip2|xz
 -r|--rotation		- set copies count what we will save (default: 10)
--i|--use-innobackupex	- will use 'innobackupex' utility to backup database
+-x|--use-xtrabackup	- will use 'xtrabackup' utility to backup database
 -m|--use-mysqldump	- will use 'mysqldump' utility to backup database
 -d|--db-only		- backing up database only without Zabbix config files etc
 -u|--db-user		- username for zabbix database
--p|--db-password	- password for database user
+-p|--db-password	- password for database user and can be path to file contains the password or set it to '-' for prompt
 -n|--db-name		- database name (default: 'zabbix')
 -h|--help		- print this help message
 -v|--version		- print version number
 --debug			- print result ingormation and exit
 
 Examples:
-# Making backup of Zabbix database and config files with innobackupex. compress it with lbzip2.
-zbx_backup --compress-with lbzip2 --use-innobackupex --db-user root --db-password P@ssw0rd
-# Making backup of Zabbix database and config files with innobackupex. compress it with lbzip2.
-zbx_backup --compress-with gzip --use-mysqldump --db-user zabbix --db-password zabbix --db-name zabbix_database
+# Making backup of Zabbix database and config files with xtrabackup. compress it with lbzip2.
+zbx_backup --compress-with lbzip2 --use-xtrabackup --db-user root --db-password P@ssw0rd
+# Making backup of Zabbix database and config files with xtrabackup. compress it with lbzip2.
+zbx_backup --compress-with gzip --use-mysqldump --db-user zabbix --db-password /root/.mysql --db-name zabbix_database
 # Making backup of Zabbix database only and compress it with xz utility.
 zbx_backup --compress-with xz --db-only -u root -p P@ssw0rd
 "
@@ -82,8 +81,18 @@ do
 			shift
 			shift
 			;;
-		"-i"|"--use-innobackupex")
-			USE_INNOBACKUPEX="YES"
+		"-x"|"-i"|"--use-xtrabackup")
+			USE_XTRABACKUP="YES"
+			shift
+			;;
+		"-s"|"--save-to")
+			DEST=$2
+			if [[ $DEST =~ \/$ ]]
+			then
+				DEST=${DEST%?}
+			fi
+			LOGFILE="$DEST/zbx_backup.log"
+			shift
 			shift
 			;;
 		"-m"|"--use-mysqldump")
@@ -142,44 +151,81 @@ then
 	DB_NAME="zabbix"
 fi
 
+# If --save-to option wasn't set, use current directory to save all data
+if ! [[ $DEST ]]
+then
+	DEST=`pwd`
+	LOGFILE="$DEST/zbx_backup.log"
+fi
+
+# Enter the password if it zero length
+if  [[ $DB_PASS == "-" ]]
+then
+	read -s -p "Please, enter the password for user '$DB_USER' ('$DB_NAME' database): " DB_PASS
+	echo -e "\n"
+fi
 
 # We cannot use both '-m' and '-i' options, so breaks here
-if [[ "$USE_INNOBACKUPEX" == "YES" ]] && [[ "$USE_MYSQLDUMP" == "YES" ]]
+if [[ "$USE_XTRABACKUP" == "YES" ]] && [[ "$USE_MYSQLDUMP" == "YES" ]]
 then
 	echo "ERROR: You cannot use '-m' and '-i' options together!"
 	exit 1
 # Also we should use at least one of them
-elif [[ "$USE_INNOBACKUPEX" != "YES" ]] && [[ "$USE_MYSQLDUMP" != "YES" ]] && [[ "$DB_ONLY" != "YES" ]]
+elif [[ "$USE_XTRABACKUP" != "YES" ]] && [[ "$USE_MYSQLDUMP" != "YES" ]] && [[ "$DB_ONLY" != "YES" ]]
 then
 	echo "ERROR: You must specify at least one database backup utility. Use '--help' to learn how."
 	exit 1
 fi
 
 # Check if username and password provided by user
-if [[ ${#DB_USER} == 0 ]] || [[ ${#DB_PASS} == 0 ]]
+if [[ ${#DB_USER} == 0 ]]
 then
-	echo "ERROR: You must provide both username and password for database '$DB_NAME'. Use '--help' to learn how."
+	echo "ERROR: You must provide username for database '$DB_NAME'. Use '--help' to learn how."
 	exit 1
 fi
 
+# The function cleans $TMP directory
+function TmpClean() {
+	if [[ -d "$TMP"  ]]
+	then
+		rm -rf $TMP/zbx_*
+	else
+		echo "WARNING: $TIMESTAMP : Cannot clean TMP directory ($TMP)." >> $LOGFILE
+	fi
+}
+
 # The function makes all backup operations
 function BackingUp() {
+	# Cleaning TMP before starting
+	TmpClean
+
 	# If '--db-only' option not set
 	if [[ "$DB_ONLY" != "YES" ]]
 	then
 		ZBX_FILES_TAR=$TMP/zbx_files_$TIMESTAMP.tar
 		# Making initial files tar archive
-		tar cf $ZBX_FILES_TAR ${ZBX_CATALOGS[0]}
+		if [[ -d ${ZBX_CATALOGS[0]} ]]
+		then
+			tar cf $ZBX_FILES_TAR ${ZBX_CATALOGS[0]}
+		else
+			echo "WARNING: $TIMESTAMP : Cannot find catalog ${ZBX_CATALOGS[0]} to save if." >> $LOGFILE
+		fi
 	
 		# Add all other catalogs in $ZBX_CATALOGS array to initial tar archive
 		if [[ -f $ZBX_FILES_TAR ]]
 		then
 			for (( i=1; i < ${#ZBX_CATALOGS[@]}; i++ ))
 			do
-				tar -rf $ZBX_FILES_TAR ${ZBX_CATALOGS[$i]}
+				if [[ -d ${ZBX_CATALOGS[$i]} ]]
+				then
+					tar -rf $ZBX_FILES_TAR ${ZBX_CATALOGS[$i]}
+				else
+					echo "WARNING: $TIMESTAMP : Cannot find catalog ${ZBX_CATALOGS[0]} to save if." >> $LOGFILE
+				fi
 			done
 		else
 			echo "ERROR: Cannot create TAR archive with zabbix data files."
+			TmpClean
 			exit 1
 		fi
 	fi
@@ -188,6 +234,7 @@ function BackingUp() {
 	if [[ $? -ne 0 ]]
 	then
 		echo "ERROR: $TIMESTAMP : Cannot create $ZBX_FILES_TAR" >> $LOGFILE
+		TmpClean
 		return 1
 	fi
 	
@@ -199,22 +246,23 @@ function BackingUp() {
 		MYSQLDUMP=`command -v mysqldump`
 		if [[ $? -eq 0 ]]
 		then
-			$MYSQLDUMP -u$DB_USER -p$DB_PASS --databases $DB_NAME > $DB_BACKUP_DST
+			$MYSQLDUMP -u$DB_USER -p$DB_PASS --databases $DB_NAME --single-transaction > $DB_BACKUP_DST
 		else
 			echo "ERROR: 'mysqldump' utility not found ($MYSQLDUMP)."
+			TmpClean
 			exit 1
 		fi
-	# If we want to use innobackupex to backup database
-	elif [[ "$USE_INNOBACKUPEX" = "YES" ]]
+	# If we want to use xtrabackup to backup database
+	elif [[ "$USE_XTRABACKUP" = "YES" ]]
 	then
 		DB_BACKUP_DST=$TMP/zbx_mysql_files_$TIMESTAMP
-		INNOBACKUPEX=`command -v innobackupex`
+		XTRABACKUP=`command -v xtrabackup`
 		if [[ $? -eq 0 ]]
 		then
-			$INNOBACKUPEX --user=$DB_USER --password=$DB_PASS --no-timestamp --parallel=4 $DB_BACKUP_DST
-			$INNOBACKUPEX --apply-log --no-timestamp $DB_BACKUP_DST
+			$XTRABACKUP --backup --user=$DB_USER --password=$DB_PASS --no-timestamp --parallel=4 --target-dir=$DB_BACKUP_DST
+			$XTRABACKUP --prepare --user=$DB_USER --password=$DB_PASS --no-timestamp --apply-log --target-dir=$DB_BACKUP_DST
 		else
-			echo "ERROR: Cannot find 'innobackupex' utility ($INNOBACKUPEX)."
+			echo "ERROR: Cannot find 'xtrabackup' utility ($XTRABACKUP)."
 			exit 1
 		fi
 	fi
@@ -223,25 +271,17 @@ function BackingUp() {
 	if [[ $? -ne 0 ]]
 	then
 		echo "ERROR: $TIMESTAMP : Cannot create database backup" >> $LOGFILE
+		TmpClean
 		return 1
 	fi
 }
 
-# The function cleans $TMP directory
-function TmpClean() {
-	if [[ -d "$TMP"  ]]
-	then
-		rm -rf "$TMP/zbx_*"
-	else
-		echo "WARNING: $TIMESTAMP : Cannot clean TMP directory ($TMP)." >> $LOGFILE
-	fi
-}
 
 # The function making rotation of old backup files
 function RotateOldCopies() {
 	# Getting old copies list and it's count
 	OLD_COPIES=(`ls -1t $DEST/zbx_backup_*`)
-	COUNT=${#old_copies[@]}
+	COUNT=${#OLD_COPIES[@]}
 
 	if [[ $COUNT -gt $ROTATION ]] && [[ $ROTATION -ne 0 ]]
 	then
@@ -280,24 +320,17 @@ then
 	else
 		printf "%-20s : %-25s\n" "Use mysqldump" "NO"
 	fi
-	if [[ "$USE_INNOBACKUPEX" == "YES" ]]
+	if [[ "$USE_XTRABACKUP" == "YES" ]]
 	then		
-		printf "%-20s : %-25s\n" "Use innobackupex" $USE_INNOBACKUPEX
+		printf "%-20s : %-25s\n" "Use xtrabackup" $USE_XTRABACKUP
 	else
-		printf "%-20s : %-25s\n" "Use innobackupex" "NO"
+		printf "%-20s : %-25s\n" "Use xtrabackup" "NO"
 	fi
 	exit 0
 fi
 
-# Cleaning TMP and Running backup operations
-TmpClean && BackingUp
-
-# Checking last exit code for backup function
-if [[ $? -ne 0 ]]
-then
-	echo "ERROR: Backup operation hasn't finished correctly. Look into log file to find posible reason ($LOGFILE)." 
-	exit 1
-fi
+# Running backup
+BackingUp
 
 # Compressing if resulted files exists
 if [[ "$USE_COMPRESSION" == "YES" ]] && [[ `command -v "$COMPRESS_WITH"` ]]
@@ -332,8 +365,11 @@ else
 	fi
 fi
 
-# Cleaning temp files and run rotation
-TmpClean && RotateOldCopies
+# Cleaning temp 
+TmpClean
+
+# Running rotation
+RotateOldCopies
 
 # Cheking and logging results
 if [[ -f "$FULL_ARC" ]]
