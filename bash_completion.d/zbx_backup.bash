e8c88cd8581cd297e8a0437dc8d8dcca6f63740b
Khatsayuk Alexander <asand3r@gmail.com>
2017-11-16 09:58:18 +0300

    Added compress utility autocompletion

--- bash_completion.d/zbx_backup.bash
+++ bash_completion.d/zbx_backup.bash
@@ -1,19 +1,38 @@
 #
 # This function provides simple autocompletion to zbx_backup script
-# Place it to /etc/bash_completion.d and run next command:
+# Place it to /etc/bash_completion.d/ and run next command:
 # . /etc/bash_completion.d/zbx_backup.bash
-# 
+# Option set is corresponds to v0.5.2 and higher.
+#
 
-_zbx_backup() {
-	local cur prev opts
-	COMPREPLY=()
-	cur="${COMP_WORDS[COMP_CWORD]}"
-	prev="${COMP_WORDS[COMP_CWORD-1]}"
-	opts="--help --version --save-to --temp-folder --compress-with --rotation --use-xtrabackup --use-mysqldump --db-only --db-user --db-password --db-name --debug"
+zbx_backup_autocomplete() {
+	# Declare local variables
+	local CUR PREV MAIN_OPTS COMPRESS_UTILS
 	
-	if [[ ${cur} == -* ]]; then
-		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+	COMPREPLY=()
+	CUR="${COMP_WORDS[COMP_CWORD]}"
+	PREV="${COMP_WORDS[COMP_CWORD-1]}"
+	MAIN_OPTS="--help --version --save-to --temp-folder --compress-with --rotation --use-xtrabackup --use-mysqldump --db-only --db-user --db-password --db-name --debug"
+	COMPRESS_UTILS="gzip bzip2 lbzip2 pbzip2 xz"
+	if [[ ${CUR} == -* ]]
+	then
+		COMPREPLY=( $(compgen -W "${MAIN_OPTS}" -- ${CUR}) )
 		return 0
 	fi
+	
+	if [[ ${PREV} == '--compress-with' ]]
+	then
+		case "${CUR}" in
+			 [a-zA-Z])
+				COMPREPLY=( $(compgen -W "${COMPRESS_UTILS}" -- ${CUR}) )
+				return 0
+				;;
+			'')
+				COMPREPLY=( $(compgen -W "${COMPRESS_UTILS}") )
+				return 0
+				;;
+		esac
+	fi
 }
-complete -F _zbx_backup zbx_backup.sh
+
+complete -F zbx_backup_autocomplete zbx_backup
