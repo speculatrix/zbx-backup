4dfdcb6bcf37691f8985fd9f7f93da757a4ac927
Khatsayuk Alexander <asand3r@gmail.com>
2017-11-17 08:27:09 +0300

    Fixed error in password checking. It fails when you set file like --db-password arument because of wrong if-then-else statement.

--- v0.5/zbx_backup_v0.5.2.sh
+++ v0.5/zbx_backup_v0.5.2.sh
@@ -106,14 +106,14 @@ do
 			;;
 		"-p"|"--db-password")
 			DB_PASS=$2
-			if [[ -f "$DB_PASS" ]] && [[ -r "$DB_PASS" ]]
+			if [[ -f "$DB_PASS" ]]
 			then
 				DB_PASS=`cat $DB_PASS`
 				if [[ $? -eq 1 ]]
 				then
 					echo "ERROR: Cannot read password from file '$DB_PASS'. Check it permissions."
+					exit 1
 				fi
-				exit 1
 			fi
 			shift
 			shift
